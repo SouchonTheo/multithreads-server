718691cca34fe63ce4ef978f3debc67a4a2dfe43
Sebeck10 <souchontheo24@gmail.com>
2021-12-15 17:58:20 +0100

    update server/client

--- Projet-SC/linda/server/LindaClient.java
+++ Projet-SC/linda/server/LindaClient.java
@@ -1,21 +1,134 @@
 package linda.server;
 
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.util.Collection;
+
 import linda.Callback;
 import linda.Linda;
 import linda.Tuple;
 
-/** Client part of a client/server implementation of Linda.
- * It implements the Linda interface and propagates everything to the server it is connected to.
- * */
+/**
+ * Client part of a client/server implementation of Linda.
+ * It implements the Linda interface and propagates everything to the server it
+ * is connected to.
+ */
 public class LindaClient implements Linda {
-	
-    /** Initializes the Linda implementation.
-     *  @param serverURI the URI of the server, e.g. "rmi://localhost:4000/LindaServer" or "//localhost:4000/LindaServer".
-     */
-    public LindaClient(String serverURI) {
-        // TO BE COMPLETED
-    }
-    
-    // TO BE COMPLETED
 
+    /**
+     * Initializes the Linda implementation.
+     * 
+     * @param serverURI the URI of the server, e.g.
+     *                  "rmi://localhost:4000/LindaServer" or
+     *                  "//localhost:4000/LindaServer".
+     */
+
+    private LindaServerImpl lindaimpl;
+
+    public LindaClient(String serverURI) {
+        try {
+            lindaimpl = (LindaServerImpl) Naming.lookup(serverURI);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        } catch (NotBoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void write(Tuple t) {
+        try {
+            lindaimpl.write(t);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public Tuple take(Tuple template) {
+        Tuple ret = null;
+        try {
+            ret = lindaimpl.take(template);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+        return ret;
+    }
+
+    @Override
+    public Tuple read(Tuple template) {
+        Tuple ret = null;
+        try {
+            ret = lindaimpl.read(template);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+        return ret;
+    }
+
+    @Override
+    public Tuple tryTake(Tuple template) {
+        Tuple ret = null;
+        try {
+            ret = lindaimpl.tryTake(template);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+        return ret;
+    }
+
+    @Override
+    public Tuple tryRead(Tuple template) {
+        Tuple ret = null;
+        try {
+            ret = lindaimpl.tryRead(template);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+        return ret;
+    }
+
+    @Override
+    public Collection<Tuple> takeAll(Tuple template) {
+        Collection<Tuple> ret = null;
+        try {
+            ret = lindaimpl.takeAll(template);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+        return ret;
+    }
+
+    @Override
+    public Collection<Tuple> readAll(Tuple template) {
+        Collection<Tuple> ret = null;
+        try {
+            lindaimpl.readAll(template);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+        return ret;
+    }
+
+    @Override
+    public void eventRegister(eventMode mode, eventTiming timing, Tuple template, Callback callback) {
+        try {
+            lindaimpl.eventRegister(mode, timing, template, callback);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void debug(String prefix) {
+        try {
+            lindaimpl.debug(prefix);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
 }
