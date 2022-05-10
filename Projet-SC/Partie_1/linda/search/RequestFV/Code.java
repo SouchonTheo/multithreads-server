1fef03d2ee69c98dbfb031ff850b3c6ade909151
Sebeck10 <souchontheo24@gmail.com>
2022-05-10 15:24:57 +0200

    VeryMegaLastV3Update

--- Projet-SC/Partie_1/linda/search/RequestFV/Code.java
+++
@@ -1,9 +0,0 @@
-package linda.search.RequestFV;
-
-public enum Code {
-    Request, // Request, UUID, String
-    Value, // Value, String //NEW : Value, UUID, String
-    Result, // Result, UUID, String, Int
-    Searcher, // Result, "done", UUID
-    IncSearchers, // IncSearchers, UUID
-}
